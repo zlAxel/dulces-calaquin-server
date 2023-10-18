06ac93886c98a88d27807622362597bed0474aae
zlAxel <axel.macias17@hotmail.com>
2023-10-17 23:24:08 -0600

    Se sube proyecto con apis de auth

--- config/app.php
+++ config/app.php
@@ -57,6 +57,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
+
     'asset_url' => env('ASSET_URL'),
 
     /*
@@ -83,7 +85,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
