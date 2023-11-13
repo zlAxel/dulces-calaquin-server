50896a1a1c3b1a1c45ca5d737c6c95aeaa6fb769
zlAxel <axel.macias17@hotmail.com>
2023-11-13 02:51:40 -0600

    Productos (Precio a integer)

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -18,7 +18,7 @@ class ProductResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
-            'price' => "$".$this->price,
+            'price' => $this->price,
             'imageUrl' => $this->image,
             // 'available' => $this->available,
         ];
