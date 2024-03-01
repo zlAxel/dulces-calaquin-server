e50b995495f987eeb6564732b51e7dee01e79c15
zlAxel <axel.macias17@hotmail.com>
2024-02-29 23:15:30 -0600

    Se suben cambios (Password security)

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -51,9 +51,9 @@ class RegisterRequest extends FormRequest
             }
             // ? Validamos que la contraseña y la confirmación de la contraseña no vayan vacías
             if ( $password_decrypt != "" && $password_confirmation_decrypt != "" ){
-                // * Validamos que la contraseña cumpla con los requisitos de seguridad
-                if ( ! preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/', $password_decrypt) ) {
-                    $validator->errors()->add('password', 'La contraseña debe tener al menos 1 letra mayúscula, 1 letra minúscula, 1 número y 1 carácter especial.');
+                // * Validamos que la contraseña cumpla con los requisitos de seguridad (1 letra mayúscula y 8 carácteres mínimo)
+                if ( ! preg_match('/^(?=.*[a-z])(?=.*[A-Z]).+$/', $password_decrypt) ) {
+                    $validator->errors()->add('password', 'La contraseña debe tener al menos 1 letra mayúscula y 8 carácteres mínimo.');
                 } else {
                     // * Validamos que la contraseña y la confirmación de la contraseña sean iguales
                     if ( $password_decrypt !== $password_confirmation_decrypt ) {
@@ -61,9 +61,15 @@ class RegisterRequest extends FormRequest
                     }
                 }
             }
-            // ? Validamos que el pin no vaya vacío
+            // ? Validamos que el pin no vaya vacío y sea númerico
             if ( $pin_decrypt == "" ){
-                $validator->errors()->add('pin', 'El pin de compras no puede ir vacío.');
+                $validator->errors()->add('pin', 'El pin de compras no puede ir vacío. ');
+            }
+            if ( ! is_numeric($pin_decrypt) ){
+                $validator->errors()->add('pin', 'El pin de compras debe ser un número. ');
+            }
+            if ( strlen($pin_decrypt) != 4 ){
+                $validator->errors()->add('pin', 'El pin de compras debe tener 4 dígitos. ');
             }
         });
     }
